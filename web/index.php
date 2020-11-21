c388b2d737fe064a0a2cd261b4b6d888278044b0
ghostvar <bagus_fahrudin_05@hotmail.com>
2020-11-21 16:53:22 +0700

    add dotenv

--- web/index.php
+++ web/index.php
@@ -1,4 +1,10 @@
 <h1>Helo World!</h1>
 
 <?php
-echo "<br config=".DATABASE_URL.">";
+
+require('../vendor/autoload.php');
+
+$dotenv = new Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo "<br config=".$_ENV['DATABASE_URL'].">";
