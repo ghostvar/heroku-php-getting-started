6f335f0c79eb323b38108dc3341ab3c4e158eee8
Didin Fakhrudin <bagus_fahrudin_05@hotmail.com>
2020-11-23 13:07:40 +0700

    nambahin db

--- web/index.php
+++ web/index.php
@@ -10,10 +10,38 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register the csanquer/pdo for database service
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(
+  new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+  array(
+    'pdo.server' => array(
+      'driver'   => 'pgsql',
+      'user' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'host' => $dbopts["host"],
+      'port' => $dbopts["port"],
+      'dbname' => ltrim($dbopts["path"], '/')
+    )
+  )
+);
+
 // Our web handlers
 
-$app->get('/', function() use($app) {
+$app->get('/', function () use ($app) {
   return 'A'; //???
+});
+
+$app->get('/db/', function() use($app) {
+  $res = $app['pdo']->prepare('SELECT name FROM test_table');
+  $res->execute();
+
+  $html = "";
+  while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+    $html += "<li>{$row['nama']}</li>";
+  }
+
+  return `<ul>`.$html.`</ul>`;
 });
 
 $app->run();
