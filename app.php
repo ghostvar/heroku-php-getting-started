5dfe50521cde1261308bb8acf1bde2f41d5c6bbb
Didin Fakhrudin <bagus_fahrudin_05@hotmail.com>
2020-11-30 17:20:40 +0700

    make code more clean

---
+++ app.php
@@ -0,0 +1,44 @@
+<?php
+require('../vendor/autoload.php');
+
+use Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Application;
+
+
+$app = new Application();
+$app['debug'] = true;
+
+// Register session
+$app->register(new SessionServiceProvider());
+
+// Register the monolog logging service
+$app->register(new MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register the csanquer/pdo for database service
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(
+  new PDOServiceProvider('pdo'),
+  array(
+    'pdo.server' => array(
+      'driver'   => 'pgsql',
+      'user' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'host' => $dbopts["host"],
+      'port' => $dbopts["port"],
+      'dbname' => ltrim($dbopts["path"], '/')
+    )
+  )
+);
+
+// Our web handlers
+
+$app->get('/', function () use ($app) {
+  return 'A'; //???
+});
+
+$app->mount('/blog', include 'handler/blogHandler.php');
+$app->mount('/db', include 'handler/dbHandler.php');
---
+++ handler/dbHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+$db = $app['controllers_factory'];
+$db->post('/', function() use($app) {
+
+$pesan = "";
+// buat nerima form
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $res = $app['pdo']->prepare("INSERT INTO test_table (name) VALUES ('{$name}')");
+  if($res->execute()) {
+    $pesan = "alert('Data tersimpan !');";
+    $_SESSION['nama'] = $name;
+  } else {
+    $pesan = "alert('Data gagal tersimpan !');";
+  }
+}
+
+return "<script>{$pesan} window.location = \"/db\";</script>";
+});
+
+$db->get('/', function() use($app) {
+  $res = $app['pdo']->prepare('SELECT name FROM test_table');
+  $res->execute();
+
+  $html = "";
+  while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+    $html .= "<li>{$row['name']}</li>";
+  }
+
+  $form = '
+    <form action="" method="post">
+      <input type="text" name="name">
+      <button name="submit" type="submit">Simpan</button>
+    </form>
+  ';
+
+  return `<ul>`.$html.`</ul>`.$form . '<br>NAME: '.@$_SESSION['nama'];
+});
+
+return $db;
