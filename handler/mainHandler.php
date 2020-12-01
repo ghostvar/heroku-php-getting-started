14b8c0e3e071a8ee085affe8b1f38a1bf36086d2
Didin Fakhrudin <bagus_fahrudin_05@hotmail.com>
2020-12-01 12:03:52 +0700

    tes use package

--- handler/mainHandler.php
+++ handler/mainHandler.php
@@ -5,8 +5,21 @@ $main = $app['controllers_factory'];
 $app->get('/login', function () use ($app) {
   return (
     "<h1>Halaman Login</h1>".
-    "<a href=\"/admin\">admin</a>"
+    "<form method=\"POST\">".
+      "<input name=\"username\" type=\"text\" placeholder=\"Username\">".
+      "<input name=\"password\" type=\"password\" placeholder=\"Password\">".
+      "<button type=\"submit\" name=\"submit\">Login</button>".
+    "</form>".
   );
+});
+
+$app->post('/login', function (Request $request) use ($app) {
+  $username = $request->get('username');
+  return $username;
+  if(/**case true */ false) {
+    return $app->redirect('/admin');
+  }
+  return $app->redirect('/login');
 });
 
 $main->get('/admin', function () use ($app) {
