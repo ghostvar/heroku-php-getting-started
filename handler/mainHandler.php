17686c46e500187256c59ec534a8ae7da691cd80
Didin Fakhrudin <bagus_fahrudin_05@hotmail.com>
2020-12-01 12:19:14 +0700

    testing session komplit

--- handler/mainHandler.php
+++ handler/mainHandler.php
@@ -5,7 +5,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 $main = $app['controllers_factory'];
 
-
 $app->get('/', function () {
   return 'Hello, Happy World!'; //???
 });
@@ -23,15 +22,28 @@ $app->get('/login', function () use ($app) {
 
 $app->post('/login', function (Request $request) use ($app) {
   $username = $request->get('username');
-  return $username;
-  if(/**case true */ false) {
+  $password = $request->get('password');
+
+  if(!empty($username) && !empty($password)) {
+    $app['session']->set('user', array('username' => $username));
     return $app->redirect('/admin');
   }
+
+  return $app->redirect('/login');
+});
+
+$main->get('/logout', function () use ($app) {
+  $app['session']->set('user', null);
   return $app->redirect('/login');
 });
 
 $main->get('/admin', function () use ($app) {
-  return "admin";
+  $user = $app['session']->get('user');
+  return (
+    "<h1>Halaman Admin</h1>".
+    "<p>Welcome {$user['username']}!</p>".
+    "<a href=\"/logout\">logout</a>"
+  );
 });
 
 $main->before($authMiddleware);
